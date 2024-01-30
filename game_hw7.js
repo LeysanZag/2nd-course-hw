edbd6592e8ab15bd48264618b3a0a550e7069856
Mona Lisa <lissicaya@gmail.com>
2024-01-29 18:21:20 -0700

    hw7 editted

--- game_hw7.js
+++ game_hw7.js
@@ -1,19 +1,23 @@
-function gameStart(){
-const words = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
-const shuffledWords = [...words].sort(() => Math.random() - 0.5);
-alert(shuffledWords);
-const userGuessFirst = prompt("Чему равнялся первый элемент массива?");
-const userGuessLast = prompt("Чему равнялся последний элемент массива?");
-if (userGuessFirst === shuffledWords[0] &&
-    userGuessLast=== shuffledWords[shuffledWords.length - 1]) {
-  alert("Поздравляем! Вы угадали!");
-} else if 
-  (userGuessFirst === shuffledWords[0] || 
-   userGuessLast === shuffledWords[shuffledWords.length - 1])
- {
-  alert("Вы были близки к победе!");
-} else {
-  alert("Вы ответили неверно. Попробуйте еще раз!");
-}
-}
+function gameStart() {
+    const words = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    const shuffledWords = [...words].sort(() => Math.random() - 0.5);
+    alert(shuffledWords);
+  
+    const userGuessFirst = prompt("Чему равнялся первый элемент массива?");
+    const userGuessLast = prompt("Чему равнялся последний элемент массива?");
+  
+    if (
+      userGuessFirst.toLowerCase() === shuffledWords[0].toLowerCase() &&
+      userGuessLast.toLowerCase() === shuffledWords[shuffledWords.length - 1].toLowerCase()
+    ) {
+      alert("Поздравляем! Вы угадали!");
+    } else if (
+      userGuessFirst.toLowerCase() === shuffledWords[0].toLowerCase() ||
+      userGuessLast.toLowerCase() === shuffledWords[shuffledWords.length - 1].toLowerCase()
+    ) {
+      alert("Вы были близки к победе!");
+    } else {
+      alert("Вы ответили неверно. Попробуйте еще раз!");
+    }
+  }
 
